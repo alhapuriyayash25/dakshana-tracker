cbbf139d6cbda71220bdd4d434cbe4e7ca90a054
Yash Alhapuriya <alhapuriyayash25@kgpian.iitkgp.ac.in>
2026-05-11 15:15:22 +0530

    Update Firebase configuration with hardcoded values
    Replaced Firebase configuration with hardcoded values.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, onValue } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBZk3FCqQhY_Iag9QZA6C8TadHpovnF9U8",
+  authDomain: "dakshana-tracker.firebaseapp.com",
+  databaseURL: "https://dakshana-tracker-default-rtdb.firebaseio.com",
+  projectId: "dakshana-tracker",
+  storageBucket: "dakshana-tracker.firebasestorage.app",
+  messagingSenderId: "611177542338",
+  appId: "1:611177542338:web:c17cc1d1c5fc90ed04415b",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
